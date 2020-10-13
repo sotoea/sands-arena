5650db14a020ec6bac8eca8654dab63705d5466e
Eduardo Soto <easm93@gmail.com>
2020-10-13 15:58:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sands_arena
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
